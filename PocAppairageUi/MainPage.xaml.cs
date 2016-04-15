dc5e795690edf9688a0ce8939945040e1c6e3bdb
Ouarzy <ouarzy@free.fr>
2016-04-15 12:03:14 +0200

    Try add watcher

--- PocAppairageUi/MainPage.xaml.cs
+++ PocAppairageUi/MainPage.xaml.cs
@@ -2,26 +2,62 @@
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Devices.Enumeration;
+using Windows.Foundation;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace PocAppairageUi
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Chercher des infos ici:
+    /// https://github.com/Microsoft/Windows-universal-samples/tree/master/Samples/DeviceEnumerationAndPairing
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string _numeroSerie;
+
         public MainPage()
         {
             InitializeComponent();
 
-            NoSerieConnecteur.Text = "+SWW387NA6QVFOJ82" + "A0007802277FF";
+            _numeroSerie = "+SWW387NA6QVFOJ82" + "A0007802277FF";
+            NoSerieConnecteur.Text = _numeroSerie;
 
-            InitWithSelector();
+            TryPairingWithWatcher();
+        }
+
+        private void TryPairingWithWatcher()
+        {
+            var watcher = DeviceInformation.CreateWatcher();
+
+            var handlerAdded = new TypedEventHandler<DeviceWatcher, DeviceInformation>(async (watch, deviceInfo) =>
+            {
+                // Since we have the collection databound to a UI element, we need to update the collection on the UI thread.
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+                {
+                    DisplayLine("Devices found");
+                    DisplayLine(deviceInfo.Id);
+                    DisplayLine(deviceInfo.Name);
+                });
+            });
+            watcher.Added += handlerAdded;
+            watcher.Stopped +=WatcherOnStopped;
+            watcher.Updated += WatcherOnUpdated;
+            watcher.Start();
+        }
+
+
+
+        private void WatcherOnStopped(DeviceWatcher sender, object args)
+        {
+            DisplayLine("stopped");
+        }
+
+        private void WatcherOnUpdated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            DisplayLine("updated:");
+            DisplayLine(sender.ToString() + args.ToString());
         }
 
         private void InitWithBleWatcher()
@@ -59,23 +95,36 @@ namespace PocAppairageUi
 
         private async void InitWithSelector()
         {
-            string selector = BluetoothDevice.GetDeviceSelector();
-            var devices = await DeviceInformation.FindAllAsync(selector);
-            foreach (var device in devices)
+            try
             {
-                var bluetoothDevice = await BluetoothDevice.FromIdAsync(device.Id);
-                if (bluetoothDevice != null)
-                {
-                    Console.Text += (bluetoothDevice.BluetoothAddress).ToString() + "\n"; 
-                }
+                var connecteur = await BluetoothDevice.FromBluetoothAddressAsync(4567890);
 
-                Console.Text += device.Id + "\n";
-
-                foreach (var property in device.Properties)
-                {
-                    Console.Text += ("   " + property.Key + " " + property.Value) + "\n";
-                }
+                DisplayLine(connecteur.DeviceId);
+                DisplayLine(connecteur.ConnectionStatus.ToString());
             }
+            catch (Exception ex)
+            {
+                DisplayLine(ex.Message);
+            }
+
+
+            //string selector = BluetoothDevice.GetDeviceSelector();
+            //var devices = BluetoothDevice.GetDeviceSelector(selector);
+            //foreach (var device in devices)
+            //{
+            //    var bluetoothDevice = await BluetoothDevice.FromIdAsync(device.Id);
+            //    if (bluetoothDevice != null)
+            //    {
+            //        DisplayLine(bluetoothDevice.BluetoothAddress.ToString());
+            //    }
+
+            //    DisplayLine(device.Id);
+
+            //    foreach (var property in device.Properties)
+            //    {
+            //        DisplayLine("   " + property.Key + " " + property.Value);
+            //    }
+            //}
         }
 
         private void DisplayLine(string message)
